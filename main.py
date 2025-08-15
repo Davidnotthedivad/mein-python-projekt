c370063416a968cb9951eacfe4a25c9007d5f00d
Davidnotthedivad <info.maehldavid@gmail.com>
2025-08-15 14:35:13 +0200

    änderungen

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # main.py
 def hallo_welt():
-    print("Hallo, Welt und Menschen!")
+    print("Hallo, Welt!")
 
 def gruss(name):
     print(f"Hallo, {name}!")
 
+def abschied(name):
+    print(f"Tschüss, {name}!")
 
 if __name__ == "__main__":
     hallo_welt()
     gruss("Python Entwickler")
-    abschied("Python Entwickler")
+    abschied("Python Entwickler:innen")
