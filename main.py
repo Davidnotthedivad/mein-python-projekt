b00340afa0661f34923cf71db7a88aa28e50294e
Davidnotthedivad <info.maehldavid@gmail.com>
2025-08-15 12:32:02 +0200

    Füge Abschied Funktion hinzu

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-# main.py
+    # main.py
 def hallo_welt():
     print("Hallo, Welt!")
 
 def gruss(name):
     print(f"Hallo, {name}!")
 
+def abschied(name):
+    print(f"Tschüss, {name}!")
+
 if __name__ == "__main__":
     hallo_welt()
     gruss("Python Entwickler")
-    
+    abschied("Python Entwickler")
